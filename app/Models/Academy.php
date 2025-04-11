e58e2e964fb49a1487c15b188ab0f79c8f241d3c
RyanBagusB <ryanbagusbimantoro@gmail.com>
2025-04-11 22:47:30 +0700

    refactor(model): rename fillable field from title to name in Academy model

--- app/Models/Academy.php
+++ app/Models/Academy.php
@@ -2,14 +2,14 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory; 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Academy extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['title', 'description'];
+    protected $fillable = ['name', 'description'];
 
     public function learningPaths()
     {
