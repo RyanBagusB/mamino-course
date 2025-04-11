fe80d3bcc9147679bcad264f7ec892f85f7f7b5f
RyanBagusB <ryanbagusbimantoro@gmail.com>
2025-04-12 06:42:00 +0700

    refactor(routes): simplify admin route definitions using resource

--- routes/web.php
+++ routes/web.php
@@ -7,21 +7,19 @@ use App\Http\Controllers\Admin\AcademyController;
 use App\Http\Controllers\Admin\LearningPathController;
 
 Route::middleware('guest')->group(function () {
-    Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+    Route::get('/login', [AuthController::class, 'showLogin']);
     Route::post('/login', [AuthController::class, 'login']);
 });
 
 Route::middleware('auth')->group(function () {
-    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+    Route::post('/logout', [AuthController::class, 'logout']);
 
-    Route::get('/', [CourseController::class, 'index'])->name('user.academies.index');
+    Route::get('/', [CourseController::class, 'index']);
+    Route::get('/learning-paths/{learningPath}', [CourseController::class, 'learningPath']);
 
-    Route::prefix('admin')->name('admin.')->group(function () {
-        Route::get('/academies', [AcademyController::class, 'index'])->name('academies.index');
-        Route::post('/academies', [AcademyController::class, 'store'])->name('academies.store');
-        Route::get('/learning-paths', [LearningPathController::class, 'index'])->name('learning-paths.index');
-        Route::post('/learning-paths', [LearningPathController::class, 'store'])->name('learning-paths.store');
-        Route::get('/learning-paths/{learningPath}/edit', [LearningPathController::class, 'edit'])->name('learning-paths.edit');
-        Route::put('/learning-paths/{learningPath}', [LearningPathController::class, 'update'])->name('learning-paths.update');
+    Route::prefix('admin')->group(function () {
+        Route::resource('academies', AcademyController::class)->except(['show', 'destroy']);
+        Route::resource('learning-paths', LearningPathController::class)->except(['show', 'destroy']);
     });
 });
+
