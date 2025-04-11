5b2108ab2a5f8d11fac049b94c6148238feb97f8
RyanBagusB <ryanbagusbimantoro@gmail.com>
2025-04-11 22:45:28 +0700

    chore(vite): exclude vendor directory from watch to improve performance

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,18 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import laravel from "laravel-vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+    server: {
+        watch: {
+            usePolling: true,
+            ignored: ["**/vendor/**"],
+        },
+    },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.jsx'],
+            input: ["resources/css/app.css", "resources/js/app.jsx"],
             refresh: true,
         }),
         react(),
